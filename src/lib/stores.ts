583a37295e5b45580a58928f7739cb3b598917fc
Marosi Gergely <gmarosi03@gmail.com>
2024-04-17 17:36:52 +0200

    feat: add store for game objects

---
+++ src/lib/stores.ts
@@ -0,0 +1,12 @@
+import { writable } from "svelte/store";
+import type { GameObject } from "$lib/types";
+
+function createObjectStore() {
+    const { subscribe, set, update } = writable(<GameObject[]>[]);
+
+    return {
+        subscribe,
+        add: (object: GameObject) => update((array) => [...array, object]),
+        empty: () => set([]),
+    }
+}
