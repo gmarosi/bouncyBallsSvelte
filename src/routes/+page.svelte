ee40d0d479b4e581cee225772ef08fcda45ff943
Marosi Gergely <gmarosi03@gmail.com>
2024-04-17 15:18:36 +0200

    feat: create interfaces for game objects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
+<script lang="ts">
+    interface Collideable {
+        x: number;
+        y: number;
+        velX: number;
+        velY: number;
+        size: number;
+
+        doesCollide: (object: Collideable) => boolean;
+    }
+
+    interface Drawable {
+        cssColor: string;
+        isFilled: boolean;
+    }
+
+    type GameObject = Collideable & Drawable & {isEvil: boolean};
+    
+    function randomInt(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function randomCSSRGB() {
+        return `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+    }
+
+    function GameObjectFactory(x: number, y: number, velX: number, velY: number, size: number, cssColor: string, isFilled: boolean, isEvil: boolean): GameObject {
+        return {
+            x,
+            y,
+            velX,
+            velY,
+            size,
+            cssColor,
+            isFilled,
+            isEvil,
+            doesCollide: (object: Collideable) => {
+                return Math.sqrt((x - object.x) ** 2 + (y - object.y) ** 2) <= size + object.size;
+            },
+        };
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
